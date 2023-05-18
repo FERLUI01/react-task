541bc2954720b0bdc23ae1d82fa24d144f220fae
FERLUI01 <freitasgluis@gmail.com>
2023-05-19 01:57:19 +0200

    added conditional styling to the reminder

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,16 +2,18 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className='task' onDoubleClick={() => onToggle(task.id)}>
-        <h3>
+    <div 
+        className={`task ${task.reminder ? 'reminder' : ''}`} 
+        onDoubleClick={() => onToggle(task.id)}
+    >
+         <h3>
             {task.text} 
             <FaTimes 
              style={{ color: 'red', cursor: 'pointer' }}
-                onClick={() => onDelete(task.id)} 
+             onClick={() => onDelete(task.id)} 
             />
          </h3>
          <p>{task.day}</p>
-
     </div>
   )
 }
