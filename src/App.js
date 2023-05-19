acf064088066864b7eb5916576bb83f7ed4b71f8
FERLUI01 <freitasgluis@gmail.com>
2023-05-19 02:49:26 +0200

    Form to add task now adds a task

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ function App() {
     },
 ])
 
+// Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+  
+
 // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -44,7 +53,7 @@ function App() {
   return (
     <div className='container'>
      <Header />
-     <AddTask />
+     <AddTask onAdd={addTask}/>
      {tasks.length > 0 ? ( 
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
         ) : (
